c7866c96a578d2061ee2f6e975b53d07f9990baf
GopeshMedayil <medayilgopesh@gmail.com>
2022-04-11 08:57:40 +0530

    Fomratted error meesage

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,5 +1,13 @@
 import { useContext } from 'react';
 import { TerminateContext } from './../../context/TerminateContext';
+import styled from 'styled-components';
+
+const Success = styled.div`
+    color:green;
+`;
+const Error = styled.div`
+    color:red;
+`;
 
 const Results = () => {
     const { result } = useContext(TerminateContext);
@@ -7,7 +15,8 @@ const Results = () => {
     return (
         <>
             <h3>Results</h3>
-            {JSON.stringify(result)}
+            {/* {JSON.stringify(result)} */}
+            {result ? result.code !== 500 ? <Success>{result.message}</Success> : <Error>{result.message}</Error> : ''}
         </>
 
     )
