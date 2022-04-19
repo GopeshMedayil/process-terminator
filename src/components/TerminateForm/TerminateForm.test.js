d0891002cf6e35001f8ccd673f2d3363a511312c
GopeshMedayil <medayilgopesh@gmail.com>
2022-04-19 23:25:58 +0530

    terminate form submit unit test cases

--- src/components/TerminateForm/TerminateForm.test.js
+++ src/components/TerminateForm/TerminateForm.test.js
@@ -1,8 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import TerminateForm from './TerminateForm';
-test('renders input field in Terminate form', async () => {
 
-    render(<TerminateForm />);
-    const inputElement = await screen.findByPlaceholderText('Enter the port');
-    expect(inputElement).toBeInTheDocument();
+describe("TerminateForm", () => {
+
+    test('renders input field in Terminate form', async () => {
+
+        render(<TerminateForm />);
+        const inputElement = await screen.findByPlaceholderText('Enter the port');
+        expect(inputElement).toBeInTheDocument();
+    });
+
+    test('renders submit button in Terminate form', async () => {
+        render(<TerminateForm />);
+        const buttonElement = await screen.findByText('Terminate');
+        expect(buttonElement).toBeInTheDocument();
+
+    });
+    test('should terminate the port', async () => {
+        render(<TerminateForm />);
+        const inputElement = await screen.findByPlaceholderText('Enter the port');
+        const buttonElement = await screen.findByText('Terminate');
+        inputElement.value = '8080';
+        buttonElement.click();
+        expect(inputElement.value).toBe('8080');
+    });
 });
